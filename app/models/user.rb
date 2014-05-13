8f3595ed8f9f52b6ccccd9f02cfa8c664ee1d64b
deepalic <deepalic@cuberoot.in>
2014-05-13 14:35:09 +0530

    Added mehod to find and create user from user hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_from_user_hash(user_hash)
+    if User.find_by_u_id(user_hash[:u_id]).present?
+      user = User.find_by_u_id(user_hash[:u_id])
+    else
+      user = User.new(user_hash)
+      user.save
+    end
+    user
+  end
 end
