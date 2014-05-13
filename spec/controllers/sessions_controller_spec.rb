1f93bdab5cd6f6fe89f2bce95523a6d6e0a03fca
deepalic <deepalic@cuberoot.in>
2014-05-13 15:44:36 +0530

    Added spec for session controller create method

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe SessionsController do
+  describe 'create action' do
+    before do
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:twitter]
+    end
 
+    it 'should create user record' do
+      expect{
+        post :create, { "oauth_token"=>"qnIWAn6XPjEFF1B43tDIQfhPM2VToi9RDYHTTdr7sE", "oauth_verifier"=>"17I63L20a3eagjuK0fzT8DxIVhvVEtl9Cxns0YncIk", "provider"=>"twitter" }, "HTTPS" => "on"
+      }.to change(User, :count).by(1)
+    end
+  end
 end
