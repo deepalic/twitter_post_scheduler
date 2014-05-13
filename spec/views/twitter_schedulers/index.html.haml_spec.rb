79748ea6cf046abcedea53a9b5309de48de96d16
deepalic <deepalic@cuberoot.in>
2014-05-13 15:55:10 +0530

    Modified for twitter_schedulers index page spec

--- spec/views/twitter_schedulers/index.html.haml_spec.rb
+++ spec/views/twitter_schedulers/index.html.haml_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'index action' do
   it 'page should contain add schedule link' do
-    visit '/'
+    visit twitter_schedulers_path
     save_and_open_page
     expect(page).to have_link 'add schedule'
   end
