a2c8dae8e5d221c21a4529ca6f6405110813af07
deepalic <deepalic@cuberoot.in>
2014-05-13 14:34:37 +0530

    Added routes for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :twitter_schedulers
   resources :home
+  resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
