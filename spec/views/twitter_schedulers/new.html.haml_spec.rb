c9fbe981f227ff1edc25a0d1bedf397439e38842
deepalic <deepalic@cuberoot.in>
2014-05-14 12:11:41 +0530

    Added view spec for twitter_scheduler new page

---
+++ spec/views/twitter_schedulers/new.html.haml_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'new action' do
+  it 'page should contain add schedule link' do
+    visit twitter_schedulers_path
+    save_and_open_page
+    expect(page).to have_link 'add schedule'
+  end
+end
