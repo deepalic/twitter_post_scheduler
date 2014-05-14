e516dca11f0cdc5127c67312cb90b67023f58f55
deepalic <deepalic@cuberoot.in>
2014-05-14 12:17:09 +0530

    Added new and create method in twitter_schedulers_controller

--- app/controllers/twitter_schedulers_controller.rb
+++ app/controllers/twitter_schedulers_controller.rb
@@ -1,5 +1,30 @@
 # Twitter scheduler controller
 class TwitterSchedulersController < ApplicationController
+  before_filter :set_twitter_scheduler, only: [:show]
   def index
+    @twitter_schedulers = TwitterScheduler.all
+  end
+
+  def new
+    @twitter_scheduler = TwitterScheduler.new
+  end
+
+  def create
+    @twitter_scheduler = TwitterScheduler.new(twitter_scheduler_params)
+    if @twitter_scheduler.save
+      redirect_to twitter_schedulers_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def twitter_scheduler_params
+    params.require(:twitter_scheduler).permit!
+  end
+
+  def set_twitter_scheduler
+    @twitter_scheduler = TwitterScheduler.find(params[:id])
   end
 end
