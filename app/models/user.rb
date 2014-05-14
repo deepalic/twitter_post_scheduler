747bed4c6d3d35ea5b2d2e88e254c047a9461a9d
deepalic <deepalic@cuberoot.in>
2014-05-14 12:15:03 +0530

    Added association between user and twitter_scheduler

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+# User class
 class User < ActiveRecord::Base
+  has_many :twitter_schedulers, dependent: :destroy
+
   def self.find_or_create_from_user_hash(user_hash)
     if User.find_by_u_id(user_hash[:u_id]).present?
       user = User.find_by_u_id(user_hash[:u_id])
