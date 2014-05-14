65c8e01b27d76d3981aa6e67ee4bf9f2289e6555
deepalic <deepalic@cuberoot.in>
2014-05-14 12:22:06 +0530

    Added before loop in home index page spec

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe 'index action' do
-  it 'page should contain button sign in with twitter' do
+  before do
     visit '/'
+  end
+
+  it 'page should contain button sign in with twitter' do
     save_and_open_page
     expect(page).to have_link 'Sign In With Twitter'
   end
