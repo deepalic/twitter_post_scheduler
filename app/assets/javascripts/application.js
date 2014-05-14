e77f2d5f986df8d101a7fd095f1d59a668081d5f
deepalic <deepalic@cuberoot.in>
2014-05-14 12:12:08 +0530

    Require jquery and datetimepicker file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery.min
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require datetimepicker/jquery.datetimepicker
+//= require custom
