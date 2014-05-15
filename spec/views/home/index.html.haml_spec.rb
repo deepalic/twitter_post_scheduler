0e2b2d5acb5273421e9dbf10ee35f847bb93b50e
deepalic <deepalic@cuberoot.in>
2014-05-15 15:01:39 +0530

    Added spec for click on sign in with twitter link

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'fakeweb'
 
 describe 'index action' do
   before do
@@ -8,5 +9,17 @@ describe 'index action' do
   it 'page should contain button sign in with twitter' do
     save_and_open_page
     expect(page).to have_link 'Sign In With Twitter'
+  end
+end
+
+describe 'click Sign In With Twitter link' do
+  it 'visit twitter auth page response with code 200' do
+    visit '/'
+    click_link 'Sign In With Twitter'
+    expect do
+      FakeWeb.register_uri(:get,
+                           'http://127.0.0.1:3000/auth/twitter',
+                           status: '200')
+    end
   end
 end
