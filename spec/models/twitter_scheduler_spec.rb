da60f4065a001bfa0dac0aa751452ebd6d155d51
deepalic <deepalic@cuberoot.in>
2014-05-15 16:23:49 +0530

    Added twitter_scheduler model spec to check valid params

--- spec/models/twitter_scheduler_spec.rb
+++ spec/models/twitter_scheduler_spec.rb
@@ -5,4 +5,9 @@ describe TwitterScheduler do
     @twitter_scheduler = { post_text: '', schedule_at: '' }
     TwitterScheduler.new(@twitter_scheduler).should_not be_valid
   end
+
+  it 'should be valid' do
+    @twitter_scheduler = { post_text: 'MyText', schedule_at: '2014-05-13 16:42:33' }
+    TwitterScheduler.new(@twitter_scheduler).should be_valid
+  end
 end
