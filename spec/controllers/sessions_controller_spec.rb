62304a946da03ca6f5adcd359a5c044ca17b0e23
deepalic <deepalic@cuberoot.in>
2014-05-15 15:00:31 +0530

    Added spec for twitter authentication

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,5 +11,12 @@ describe SessionsController do
         post :create, { "oauth_token"=>"qnIWAn6XPjEFF1B43tDIQfhPM2VToi9RDYHTTdr7sE", "oauth_verifier"=>"17I63L20a3eagjuK0fzT8DxIVhvVEtl9Cxns0YncIk", "provider"=>"twitter" }, "HTTPS" => "on"
       }.to change(User, :count).by(1)
     end
+
+    it 'should create authentication' do
+      lambda do
+        post :create, provider: 'twitter'
+        response.should redirect_to(twitter_schedulers_path)
+      end
+    end
   end
 end
