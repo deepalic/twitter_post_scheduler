f223b5efd5e321a448a84576823ffb59336343cc
deepalic <deepalic@cuberoot.in>
2014-05-15 16:24:24 +0530

    Added user model spec to check valid params

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,11 @@ describe User do
               auth_token: '', auth_secret: '' }
     User.new(@user).should_not be_valid
   end
+
+  it 'should be valid' do
+    @user = { u_id: '122324', nickname: 'acd', name: 'asd df',
+              profile_image: 'image.jpg', url: '/twitter/acd',
+              auth_token: 'tqweyrw273682', auth_secret: 'DFVD2387cmxn' }
+    User.new(@user).should be_valid
+  end
 end
