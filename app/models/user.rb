044a86ec9aa42e2625c7a805f0a82d1865cc3d9c
deepalic <deepalic@cuberoot.in>
2014-05-15 12:10:28 +0530

    Added presence check on user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@
 class User < ActiveRecord::Base
   has_many :twitter_schedulers, dependent: :destroy
 
+  validates_presence_of :u_id, :nickname, :name, :profile_image, :url,
+                        :auth_token, :auth_secret
+
   def self.find_or_create_from_user_hash(user_hash)
     if User.find_by_u_id(user_hash[:u_id]).present?
       user = User.find_by_u_id(user_hash[:u_id])
