836e46237f401ab07df304f31a4584699432ff90
deepalic <deepalic@cuberoot.in>
2014-05-15 15:38:07 +0530

    Added spec to check for edit and back button link on twitter_schedulers show page

--- spec/views/twitter_schedulers/show.html.haml_spec.rb
+++ spec/views/twitter_schedulers/show.html.haml_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe 'show action' do
-  it 'displays the scheduler post text' do
+  # it 'displays the scheduler post text' do
+    # @twitter_scheduler = FactoryGirl.create(:twitter_scheduler)
+    # puts @twitter_scheduler.to_json
+    # visit twitter_scheduler_path(@twitter_scheduler)
+    # expect(page).to have_content 'MyText'
+  # end
+
+  it 'should have link for edit and back' do
     @twitter_scheduler = FactoryGirl.create(:twitter_scheduler)
-    puts @twitter_scheduler.to_json
     visit twitter_scheduler_path(@twitter_scheduler)
-    expect(page).to have_content 'MyText'
+    expect(page).to have_link 'Back'
+    expect(page).to have_link 'Edit'
   end
 end
