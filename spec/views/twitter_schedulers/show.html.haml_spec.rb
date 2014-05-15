1cd80ca672acb1a9a65646b0e6d545f0ebc33c1b
deepalic <deepalic@cuberoot.in>
2014-05-15 17:26:01 +0530

    Modified show action spec for twitter_schedulers

--- spec/views/twitter_schedulers/show.html.haml_spec.rb
+++ spec/views/twitter_schedulers/show.html.haml_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe 'show action' do
-  # it 'displays the scheduler post text' do
-    # @twitter_scheduler = FactoryGirl.create(:twitter_scheduler)
-    # puts @twitter_scheduler.to_json
-    # visit twitter_scheduler_path(@twitter_scheduler)
-    # expect(page).to have_content 'MyText'
-  # end
+  it 'displays the scheduler post text' do
+    @twitter_scheduler = FactoryGirl.create(:twitter_scheduler)
+    visit twitter_scheduler_path(@twitter_scheduler)
+    page.html.should include('MyText')
+  end
 
   it 'should have link for edit and back' do
     @twitter_scheduler = FactoryGirl.create(:twitter_scheduler)
