5af3efad09db8af1e91f7c3a2d6b20ee7147cfc1
deepalic <deepalic@cuberoot.in>
2014-05-16 14:17:11 +0530

    Added state machine for status field of twitter_scheduler model

--- app/models/twitter_scheduler.rb
+++ app/models/twitter_scheduler.rb
@@ -3,4 +3,15 @@ class TwitterScheduler < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :post_text
   validates_presence_of :schedule_at
+
+  state_machine :status, initial: :scheduled do
+    event :success do
+      transition [:scheduled] => :success
+    end
+    event :failed do
+      transition [:scheduled] => :failed
+    end
+
+    state :scheduled, :success, :failed
+  end
 end
