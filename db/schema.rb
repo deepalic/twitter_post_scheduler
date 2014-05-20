1645054faf797844c01460fab96888f411fb4652
deepalic <deepalic@cuberoot.in>
2014-05-20 12:36:30 +0530

    Modified schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516071243) do
+ActiveRecord::Schema.define(version: 20140520060147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 20140516071243) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "status"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "users", force: true do |t|
